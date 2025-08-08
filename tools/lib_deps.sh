11e726788daca3d817007ad2506b6a45bf1e64ae
andriansah <andriansah@yahoo.com>
2025-08-09 00:49:55 +0700

    hapus .DS_Store

--- tools/lib_deps.sh
+++ tools/lib_deps.sh
@@ -1,1 +1,1 @@
-npm i better-sqlite3 require-from-string mqtt crypto-js highlight.js cookie-parser prompt-sync --save
+npm i mysql2 better-sqlite3 require-from-string mqtt crypto-js highlight.js cookie-parser prompt-sync --save
