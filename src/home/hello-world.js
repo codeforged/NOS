9676f5f8e6e65442f1045ff29a7e12950534a641
andriansah <andriansah@yahoo.com>
2025-08-09 00:53:23 +0700

    Update hello-world.js

--- src/home/hello-world.js
+++ src/home/hello-world.js
@@ -1,25 +1,8 @@
 module.exports = {
   name: "hello world",
   version: "0.01",
-  needRoot: false,
-  main: function (nos) {
-    this.shell.loadDevices(
-      [
-        { name: __APP.defaultComm, objectName: "mqtnl" },
-        { name: "bfsAccess", objectName: "fa" },
-      ],
-      this,
-    );
-
+  main: function () {
     this.crt.textOut("Hello World");
     this.terminate();
-
-    this.abc = function () {
-      this.crt.textOut("This is a test function.");
-      for (let i = 0; i < 5; i++) {
-        this.crt.textOut(`Counter: ${i}`);
-      }
-    }
-  }
+  },
 };
-
